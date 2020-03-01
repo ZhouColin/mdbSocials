2140c9c304858b1fe23eb79085d0ae7957d79bbe
Maggie Yi <myi_gomboc@berkeley.edu>
2020-02-29 17:57:45 -0800

    NewSocialVC

--- mdbSocials/mdbSocials/Controller/LoginViewController.swift
+++ mdbSocials/mdbSocials/Controller/LoginViewController.swift
@@ -6,40 +6,63 @@
 //  Copyright © 2020 Colin Zhou. All rights reserved.
 //
 
-import Foundation
 import UIKit
 import Firebase
 
-class LoginViewController : UIViewController {
-    @IBOutlet weak var loginStack: UIStackView!
-    @IBAction func loginTapped(_ sender: Any) {
-        navigateToInterface()
-        performSegue(withIdentifier: "goHome", sender: self)
-    }
+class LoginViewController: UIViewController {
+    @IBOutlet weak var logo: UIImageView!
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
+    
+    @IBOutlet weak var passwordTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
         assignbackground()
     }
     
-
     public func assignbackground(){
-          let background = UIImage(named: "background1.png")
-          var imageView : UIImageView!
-          imageView = UIImageView(frame: view.bounds)
-        imageView.contentMode =  UIView.ContentMode.scaleAspectFill
-          imageView.clipsToBounds = true
-          imageView.image = background
-          imageView.center = view.center
-          view.addSubview(imageView)
-          self.view.sendSubviewToBack(imageView)
-      }
-    
-    private func navigateToInterface() {
-        let loginStoryboard = UIStoryboard(name: "Login", bundle: Bundle.main)
-        guard let SignupViewController = loginStoryboard.instantiateViewController(identifier: "Signup") as? SignupViewController else {
-            return
+            let background = UIImage(named: "background1.png")
+            var imageView : UIImageView!
+            imageView = UIImageView(frame: view.bounds)
+            imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+            imageView.clipsToBounds = true
+            imageView.image = background
+            imageView.center = view.center
+            view.addSubview(imageView)
+            self.view.sendSubviewToBack(imageView)
         }
-        //segue
-        present(SignupViewController, animated: true, completion: nil)
+    
+    func handleLogin() {
+        let email = usernameTextField.text!
+        let password = passwordTextField.text!
+        
+        let auth = Auth.auth()
+        auth.signIn(withEmail: email, password: password, completion: { (user, error) in
+            guard error == nil else {
+                self.displayAlert(title: "Error", message: "Username or Password Incorrrect")
+                print(error.debugDescription)
+                return
+            }
+            guard user != nil else {
+                self.displayAlert(title: "Error", message: "User Error")
+                return
+            }
+            self.performSegue(withIdentifier: "loginToFeed", sender: self)
+        })
     }
+    
+    func displayAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(defaultAction)
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    @IBAction func loginButton(_ sender: Any) {
+        handleLogin()
+    }
+    
 }
--- mdbSocials/mdbSocials/Controller/SignupViewController.swift
+++ mdbSocials/mdbSocials/Controller/SignupViewController.swift
@@ -4,44 +4,96 @@
 //
 //  Created by Maggie Yi on 2/28/20.
 //
-
-import Foundation
 import UIKit
 import Firebase
+import Foundation
 
 class SignupViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-    }
-    
+
+    @IBOutlet weak var logo: UIImageView!
+    @IBOutlet weak var fullnameLabel: UILabel!
+    @IBOutlet weak var usernameLabel: UILabel!
     @IBOutlet weak var emailLabel: UILabel!
     @IBOutlet weak var passwordLabel: UILabel!
-    @IBOutlet weak var userNameTextField: UITextField!
+    
+    @IBOutlet weak var fullnameTextField: UITextField!
+    
+    @IBOutlet weak var usernameTextField: UITextField!
     @IBOutlet weak var passwordTextField: UITextField!
-    @IBAction func loginTapped(_ sender: Any) {
-        loadUserData()
+    
+    @IBOutlet weak var emailTextField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        assignbackground()
+
     }
     
+    public func assignbackground(){
+            let background = UIImage(named: "background1.png")
+            var imageView : UIImageView!
+            imageView = UIImageView(frame: view.bounds)
+            imageView.contentMode =  UIView.ContentMode.scaleAspectFill
+            imageView.clipsToBounds = true
+            imageView.image = background
+            imageView.center = view.center
+            view.addSubview(imageView)
+            self.view.sendSubviewToBack(imageView)
+        }
     
-    private func loadUserData() {
+    func handleSignup() {
+        let fullname = fullnameTextField.text!
+        let username = usernameTextField.text!
+        let password = passwordTextField.text!
+        let email = emailTextField.text!
+        
+        if password.count < 8 {
+            self.displayAlert(title: "Error", message: "Password must be 8 characters or longer")
+        }
+        
+        // init auth
         let auth = Auth.auth()
-        auth.signIn(withEmail: userNameTextField.text!, password: passwordTextField.text!) {(user, error) in
+        // Create User
+        auth.createUser(withEmail: email, password: password) { (user, error) in
             guard error == nil else {
-                self.displayError("Error")
+                self.displayAlert(title: "Error", message: "Check if textfields are valid")
+                print(error.debugDescription)
                 return
             }
             guard user != nil else {
-                self.displayError("Error")
+                self.displayAlert(title: "Error", message: "User Error")
                 return
             }
+            
+        // Enter new node into database
+                // a. Database Reference
+            let db = Database.database().reference()
+                // b. Users Folder
+            let usersNode = db.child("Users")
+            
+                // c. New Node Reference
+            guard let newUserId = usersNode.childByAutoId().key else {
+                return
+            }
+            let userNode = usersNode.child(newUserId)
+            userNode.updateChildValues(["username": username, "name": fullname])
+            
+                // 3. Segue to Feed
+            self.performSegue(withIdentifier: "signupToFeed", sender: self)
         }
         
     }
     
-    func displayError(_ message:String) {
-            let alertView = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
-            alertView.addAction(UIAlertAction(title: "Ok", style: .cancel, handler: nil))
-            self.present(alertView, animated: true, completion: nil)
+    func displayAlert(title: String, message: String) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        let defaultAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+        alert.addAction(defaultAction)
+        self.present(alert, animated: true, completion: nil)
     }
+    
+    @IBAction func signupButton(_ sender: Any) {
+        handleSignup()
+    }
+    
     
 }
